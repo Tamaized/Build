59896456bacef85b19ace9911b784a8be61c603c
Tamaized <Tamaized@users.noreply.github.com>
2017-10-21 20:59:26 -0700

    cleanup (#1)

--- src/Tamaized/Build/Build.java
+++ src/Tamaized/Build/Build.java
@@ -13,7 +13,6 @@ public class Build {
 
 	public static void main(String[] args) {
 		System.out.println("Starting");
-
 		if (args.length > 1) {
 			System.out.println("Looking for file: " + args[0]);
 			File file = new File(args[0]);
@@ -24,7 +23,6 @@ public class Build {
 				try {
 					BufferedReader br = new BufferedReader(new FileReader(file));
 					String line = br.readLine();
-
 					while (line != null) {
 						if (line.length() >= varLen && line.substring(0, varLen).equals(args[1])) {
 							String test = line.substring(varLen + 1);
@@ -37,7 +35,7 @@ public class Build {
 					br.close();
 				} catch (IOException e) {
 					e.printStackTrace();
-				} catch(NumberFormatException e){
+				} catch (NumberFormatException e) {
 					System.out.println("The Variable does not have a set positive int value; Stopping");
 					System.exit(0);
 				}
@@ -46,27 +44,21 @@ public class Build {
 					System.out.println("Variable found! Increasing to: " + value);
 					try {
 						List<String> fileContent = new ArrayList<>(Files.readAllLines(file.toPath(), StandardCharsets.UTF_8));
-						for (int i = 0; i < fileContent.size(); i++) {
+						for (int i = 0; i < fileContent.size(); i++)
 							if (fileContent.get(i).length() >= varLen && fileContent.get(i).substring(0, varLen).equals(args[1])) {
-								fileContent.set(i, fileContent.get(i).substring(0, varLen+1).concat(String.valueOf(value)));
+								fileContent.set(i, fileContent.get(i).substring(0, varLen + 1).concat(String.valueOf(value)));
 								break;
 							}
-						}
 						Files.write(file.toPath(), fileContent, StandardCharsets.UTF_8);
 						System.out.println("Finished!");
 					} catch (IOException e) {
 						e.printStackTrace();
 					}
-				} else {
+				} else
 					System.out.println("Could not find the variable");
-				}
-
-			} else {
+			} else
 				System.out.println("Could not find file");
-			}
-		} else {
+		} else
 			System.out.println("Not enough Arguments");
-		}
-
 	}
 }
